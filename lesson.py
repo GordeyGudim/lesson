21de52c3c12267861501fe4abd79c20f23e1608a
GudimGordey <gordey.gudim@mail.ru>
2024-02-02 13:25:08 +0300

    homework

--- lesson.py
+++ lesson.py
@@ -1,2 +1,2 @@
-lst = [0, 1, 0, 2, 3, 2, 0, 1]
-print(f'minimum:{min(lst)}\nmaximum:{max(lst)}\nsum:{sum(lst)}\nsorted:{sorted(lst, reverse=True)}\ntuple:{tuple(lst)}')
+ls = [0, 1, 0, 2, 3, 2, 0, 1]
+print(f'minimum:{min(ls)}\nmaximum:{max(ls)}\nsum:{sum(ls)}\nsorted:{sorted(ls, reverse=True)}\ntuple:{tuple(ls)}')
